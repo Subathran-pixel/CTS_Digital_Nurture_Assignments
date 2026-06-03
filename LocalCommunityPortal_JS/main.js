3f8e7e575ab2018c0781b54385aeedcc6dd0a830
Subathran <subathran31@gmail.com>
2026-06-03 14:05:10 +0530

    Implemented JavaScript Exercise 2 data types and operators

--- LocalCommunityPortal_JS/main.js
+++ LocalCommunityPortal_JS/main.js
@@ -3,3 +3,20 @@ console.log("Welcome to the Community Portal");
 window.addEventListener("load", function () {
     alert("Page Fully Loaded");
 });
+
+/* Exercise 2 */
+
+const eventName = "Music Festival";
+const eventDate = "15-06-2026";
+let availableSeats = 100;
+
+console.log(`
+Event Name : ${eventName}
+Event Date : ${eventDate}
+Available Seats : ${availableSeats}
+`);
+
+// Simulate registration
+availableSeats--;
+
+console.log("Seats Left After Registration : " + availableSeats);
